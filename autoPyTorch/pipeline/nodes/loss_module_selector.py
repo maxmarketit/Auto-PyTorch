59fd19fe46a1f1ff477bb605d17b1f5f795b2417
maxmarketit <max@marketit.asia>
2020-06-24 01:23:43 +0000

    loss_module added

--- autoPyTorch/pipeline/nodes/loss_module_selector.py
+++ autoPyTorch/pipeline/nodes/loss_module_selector.py
@@ -13,6 +13,95 @@ from torch.nn.modules.loss import _Loss
 from autoPyTorch.utils.configspace_wrapper import ConfigWrapper
 from autoPyTorch.utils.config.config_option import ConfigOption
 
+class QuantileLoss(_Loss):
+    __constants__ = ['reduction']
+
+    def __init__(self, size_average=None, reduce=None, reduction='mean', qs=(0.5,)):
+        super(QuantileLoss, self).__init__(size_average, reduce, reduction)
+        if not(isinstance(qs, tuple)) and not(isinstance(qs, np.ndarray)):
+            raise ValueError('qs must be either list or np.array')
+        if isinstance(qs, tuple):
+            qs = np.array(qs)
+        if isinstance(qs, np.ndarray) and not (len(qs.shape) == 1):
+            raise ValueError('qs must be shape of 1 dim')
+
+        self.qs = qs
+
+    def forward(self, input, target):
+        reduction = self.reduction
+        if not (target.size() == input.size()):
+            warnings.warn("Using a target size ({}) that is different to the input size ({}). "
+                          "This will likely lead to incorrect results due to broadcasting. "
+                          "Please ensure they have the same size.".format(target.size(), input.size()),
+                          stacklevel=2)
+            
+        qs = torch.tensor(self.qs, dtype = input.dtype, requires_grad=False).to(input.device)
+        if True:
+            e = target - input           
+            ret = torch.max(qs * e, (qs - 1) * e)
+        if reduction != 'none':
+            ret = torch.mean(ret) if reduction == 'mean' else torch.sum(ret)
+        else:
+            raise ValueError('not(target.requires_grad): not yet implemented')
+
+class QinputLoss(_Loss):
+    # import numpy as np
+    import warnings
+
+    __constants__ = ['reduction']
+
+    def __init__(self, size_average=None, reduce=None, reduction='mean', ws=(0,1)):
+            # size_average와 reduce는 앞으로 deprecated될 예정, reduction을 사용하라
+            # reduction = 'mean' or 'sum' or 'none'
+        super(QinputLoss, self).__init__(size_average, reduce, reduction)
+        if not(isinstance(ws, tuple)) and not(isinstance(ws, np.ndarray)):
+            raise ValueError('ws must be either list or np.array')
+        if isinstance(ws, tuple):
+            ws = np.array(ws)
+        if isinstance(ws, np.ndarray) and not (len(ws.shape) == 1):
+            raise ValueError('qs must be shape of 1 dim')
+
+        self.ws = ws
+
+    def forward(self, input, target):
+        # 위의 l1_loss에서 따옴
+        # l1_loss(input, target, size_average=None, reduce=None, reduction='mean')
+        reduction = self.reduction
+        #if not torch.jit.is_scripting():  # 뭐 하는 곳인지 모름
+        #    raise ValueError('QuantileLoss: torch.jit.is_scripting() not implemented')
+        #    #tens_ops = (input, target)
+        #    #if any([type(t) is not Tensor for t in tens_ops]) and has_torch_function(tens_ops):
+        #    #    return handle_torch_function(
+        #    #        l1_loss, tens_ops, input, target, size_average=size_average, reduce=reduce,
+        #    #        reduction=reduction)
+        if not (target.size() == input.size()):
+            warnings.warn("Using a target size ({}) that is different to the input size ({}). "
+                          "This will likely lead to incorrect results due to broadcasting. "
+                          "Please ensure they have the same size.".format(target.size(), input.size()),
+                          stacklevel=2)
+            
+        ws = torch.tensor(self.ws, dtype = input.dtype, requires_grad=False).to(input.device)
+        # ws : weights on quantile loss and weights on output 
+        w_q = ws[0]
+        w_input = ws[1:].reshape(1,-1)
+        q = target[:,0:1]
+        #if target.requires_grad: # 아마 target이 고정되어 있는 경우 else:에서 빠른 처리가 가능한 듯.
+        if True:
+            e_q = torch.abs(target[:,0]-input[:,0]) # q error, if w_q is 0, ignored
+            e= target[:,1:] - input[:,1:] # target error
+            
+            # target[:,0:1] is q! 
+            ret = w_input*torch.max(q * e, (q - 1) * e)
+            #ret = torch.abs(input - target) # MAE 
+        if reduction != 'none':
+            ret = torch.mean(ret) +torch.mean(w_q* e_q) if reduction == 'mean' else torch.sum(ret) + torch.sum(w_q*e_q)
+        else:
+            raise ValueError('not(target.requires_grad): not yet implemented')
+            #expanded_input, expanded_target = torch.broadcast_tensors(input, target)
+            #ret = torch._C._nn.l1_loss(expanded_input, expanded_target, _Reduction.get_enum(reduction))
+        return ret 
+
+
 
 class LossModuleSelector(PipelineNode):
     def __init__(self):
